24041caafa1e7c0e55cdb827a42b7c65e7aa80af
Juan Valdivia <deviaan2@gmail.com>
2023-08-10 22:27:05 -0600

    initial movement code

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float _horizontal;
+    private const float Speed = 8f;
+    private const float JumpPower = 16f;
+    private bool _isFacingRight = true;
+
+    void Update()
+    {
+        rb.velocity = new Vector2(_horizontal * Speed, rb.velocity.y);
+        if (!_isFacingRight && _horizontal > 0f)
+        {
+           Flip(); 
+        }
+        else if(_isFacingRight && _horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpPower);
+        }
+        
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpPower * 0.5f);
+        }
+    }
+
+    private void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _horizontal = context.ReadValue<Vector2>().x;
+    }
+}
