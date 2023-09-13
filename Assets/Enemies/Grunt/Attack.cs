83621c63a5edc3342e83af4eac68b34ce6696362
James Sargent <jsargent3112@gmail.com>
2023-09-13 16:46:08 +0100

    Added clown to level design
    Added 2D tilemap collisions to floor and walls to test player

---
+++ Assets/Enemies/Grunt/Attack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+	private Animator animator;
+	private EnemyMovement enemyMovement;
+	private GameObject player;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		enemyMovement = GetComponent<EnemyMovement>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 playerDirection = player.transform.position - transform.position;
+
+		if (playerDirection.magnitude < enemyMovement.attackRange)
+		{
+			animator.SetBool("isAttacking", true);
+		}
+		else
+		{
+			animator.SetBool("isAttacking", false);
+		}
+	}
+}
---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    [SerializeField] private float force = 10f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = new Vector2(force, 0);
+    }
+}
