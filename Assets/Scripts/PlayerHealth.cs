2bb8b97132307baace0c7acba2b0cc49101f7fee
Juan Valdivia <deviaan2@gmail.com>
2023-09-16 11:42:09 -0600

    fix bounds

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] public int playerHealth = 5;
+
+    public void HitPlayer()
+    {
+        --playerHealth;
+        if (playerHealth <= 0)
+        {
+            // Todo: death and hit animations
+            Destroy(gameObject);
+        }
+    }
+}
