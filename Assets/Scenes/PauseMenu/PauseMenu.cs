1e429e1e26b64c3d14037af23b96f455a06d4795
Billy Hawkes <billyhawkes02@gmail.com>
2023-09-17 18:59:55 -0400

    Main menu and pause menu

---
+++ Assets/Scenes/PauseMenu/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+	[SerializeField] GameObject pauseMenu;
+	[SerializeField] private bool paused = false;
+
+	public void TogglePause(InputAction.CallbackContext context)
+	{
+		Debug.Log("TogglePause");
+		if (context.performed)
+		{
+			paused = !paused;
+			if (paused)
+			{
+				Instantiate(pauseMenu);
+				Time.timeScale = 0f;
+			}
+			else
+			{
+				Destroy(GameObject.Find("PauseMenu(Clone)"));
+				Time.timeScale = 1f;
+			}
+
+		}
+	}
+
+	public void Home(int sceneID)
+	{
+		Time.timeScale = 1f; // need to set to 1 as well
+		SceneManager.LoadScene(sceneID);
+	}
+}
