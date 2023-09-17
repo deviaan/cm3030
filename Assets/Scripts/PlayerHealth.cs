c66422174ab3cf47f4b810bf83c834a1636f06fc
James Sargent <jsargent3112@gmail.com>
2023-09-17 01:44:04 +0100

    Changed steps into blocks
    bugs with steps so changed them into platforms

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] public int playerHealth = 5;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Damage"))
+        {
+            --playerHealth;
+            if (playerHealth <= 0)
+            {
+                // Todo: death and hit animations
+                Destroy(gameObject);
+            }
+        }
+    }
+}
